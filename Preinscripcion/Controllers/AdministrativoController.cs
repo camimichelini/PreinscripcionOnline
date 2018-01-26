df5d42483d7a66a09857b79a225ffae8eda577ee
camimichelini <35546057+camimichelini@users.noreply.github.com>
2018-01-26 17:46:23 -0300

    Controador y vistas Administrativo

---
+++ Preinscripcion/Controllers/AdministrativoController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Preinscripcion.Controllers
+{
+    public class AdministrativoController : Controller
+    {
+        // GET: Administrativo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Administrativo/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Administrativo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Administrativo/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Administrativo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Administrativo/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Administrativo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Administrativo/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult FinInscripcion()
+        {
+            return View();
+        }
+
+        public ActionResult BuscarDoc()
+        {
+            return View();
+        }
+    }
+}
