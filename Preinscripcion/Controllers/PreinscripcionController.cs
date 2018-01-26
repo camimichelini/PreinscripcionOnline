937aff5a235722d0a464f720d5dd7247af677c22
pauladelgiorgio <35787227+pauladelgiorgio@users.noreply.github.com>
2018-01-26 11:42:23 -0300

    Pantallas Pau

---
+++ Preinscripcion/Controllers/PreinscripcionController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Preinscripcion.Controllers
+{
+    public class PreinscripcionController : Controller
+    {
+        // GET: Preinscripcion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Preinscripcion/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Preinscripcion/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Preinscripcion/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Preinscripcion/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Preinscripcion/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Preinscripcion/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Preinscripcion/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Fin preinscripcion
+        public ActionResult FinPreinscripcion()
+        {
+            return View();
+        }
+    }
+}
---
+++ Preinscripcion/Controllers/TurnoController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Preinscripcion.Controllers
+{
+    public class TurnoController : Controller
+    {
+        // GET: Turno
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Turno/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Turno/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Turno/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Turno/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Turno/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Turno/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Turno/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ModificarTurnoOk
+        public ActionResult ModificarTurnoOk()
+        {
+            return View();
+        }
+
+        // GET: CancelarTurnoOk
+        public ActionResult CancelarTurnoOk()
+        {
+            return View();
+        }
+    }
+}
