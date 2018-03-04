2366d91bd46d05bbc2dee842789e6e9ce8f45554
camimichelini <35546057+camimichelini@users.noreply.github.com>
2018-03-04 00:58:58 -0300

    Cambios formulario y clase alumno

--- Entidades/Alumno.cs
+++ Entidades/Alumno.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Preinscripcion.Entidades
 {
@@ -13,9 +14,11 @@ namespace Preinscripcion.Entidades
     public class Alumno : Persona
     {
 
-        [Key]
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)] // PK autoincremental (Identity column)
+        //public int AlumnoId { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // PK autoincremental (Identity column)
         public Int32 Legajo { get; set; }
-        public int AlumnoId { get; set; }
         public int Telefono { get; set; }
         public int Celular { get; set; }
         public string Mail { get; set; }
@@ -23,6 +26,7 @@ namespace Preinscripcion.Entidades
         public string NomyApePMT { get; set; }
         public string NombreColegio { get; set; }
         public string TituloColegio { get; set; }
+        public string  Emancipacion { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
@@ -35,19 +39,22 @@ namespace Preinscripcion.Entidades
         public int SexoId { get; set; }
         public virtual Sexo Sexo { get; set; }
         public int Localidad1Id { get; set; }
-        public virtual Localidad LugarNacimiento { get; set; }
+        //public virtual Localidad LugarNacimiento { get; set; }
         public int Localidad2Id { get; set; }
-        public virtual Localidad LugarDomicilio { get; set; }
+        //public virtual Localidad LugarDomicilio { get; set; }
         public int Provincia1Id { get; set; }
-        public virtual Provincia ProvinciaNacimiento { get; set; }
+        //public virtual Provincia ProvinciaNacimiento { get; set; }
         public int Provincia2Id { get; set; }
-        public virtual Provincia ProvinciaDomicilio { get; set; }
+        //public virtual Provincia ProvinciaDomicilio { get; set; }
         public int CarreraId { get; set; }
         public virtual Carrera Carrera { get; set; }
 
-        public Boolean Emancipacion { get; set; }
 
-        
+        //VER ARCHIVOS
+        //public FileStream FotoCarnet { get; set; }
+        //public FileStream FotocDoc { get; set; }
+        //public FileStream FotocAnalitico { get; set; }
+        //public FileStream CertifTrabajo { get; set; }
 
     }
 }
