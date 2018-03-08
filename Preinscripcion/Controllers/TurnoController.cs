6449a82f396be15943be926d8b735f0b7c7d5c34
pauladelgiorgio <35787227+pauladelgiorgio@users.noreply.github.com>
2018-03-08 19:10:11 -0300

    Turnera

--- Preinscripcion/Controllers/TurnoController.cs
+++ Preinscripcion/Controllers/TurnoController.cs
@@ -21,50 +21,130 @@ namespace Preinscripcion.Controllers
 
         public ActionResult ModificarTurno(int? id)
         {
-            var tur = db.Turno.Where(b => b.TurnoId == id).FirstOrDefault();
+            var preins = db.PreinscripcionOnline.Where(p => p.PreinscripcionId == id).FirstOrDefault();
+            var turN = db.Turno.Where(f => f.Estado == true).Select(f => f.Fecha); ;
+
+            turN = turN.Distinct();
+
+
             string fecha;
             string hora;
-            fecha = tur.Fecha;
-            hora = tur.Hora;
+
+            var t = db.Turno.Where(a => a.TurnoId == preins.TurnoId).FirstOrDefault();
+
+            fecha = t.Fecha;
+            hora = t.Hora;
 
 
 
-            ViewData["Fecha"] = fecha;
-            ViewData["Hora"] = hora;
-            ViewData["TurnoId"] = id;
+            ViewBag.Fecha = new SelectList(turN, "Fecha");
+            ViewBag.Hora = new SelectList(db.Turno, "TurnoId", "Hora");
 
-            return View();
-            
+            ViewData["FechaTur"] = fecha;
+            ViewData["HoraTur"] = hora;
+            ViewData["PreinscripcionID"] = id;
+
+            return View(); ;
+
         }
 
 
-        
-        public ActionResult CancelarTurno(int id)
+
+        public ActionResult CancelarTurno(int? id)
         {
-            var tur = db.Turno.Where(b => b.TurnoId == id).FirstOrDefault(); 
+            var preins = db.PreinscripcionOnline.Where(p => p.PreinscripcionId == id).FirstOrDefault();
             string fecha;
             string hora;
-            fecha = tur.Fecha;
-            hora = tur.Hora;
-            
+
+            var t = db.Turno.Where(a => a.TurnoId == preins.TurnoId).FirstOrDefault();
+
+            fecha = t.Fecha;
+            hora = t.Hora;
+
 
 
             ViewData["Fecha"] = fecha;
             ViewData["Hora"] = hora;
-            ViewData["TurnoId"] = id;
+            ViewData["PreinscripcionID"] = id;
 
             return View();
         }
 
-        public ActionResult Turnera()
+        public ActionResult Turnera(int id)
         {
             var p = db.Turno.Where(f => f.Estado == true).Select(f => f.Fecha); ;
             
             p= p.Distinct();
             ViewBag.Fecha = new SelectList(p,  "Fecha");
             ViewBag.Hora = new SelectList(db.Turno, "TurnoId", "Hora");
+            ViewData["PersonaId"] = id;
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Turnera([Bind(Include = "PersonaId,TurnoID")] PreinscripcionOnline preinscripcion, int hora, string fecha, int id)
+        {
+            string message = string.Empty;
+
+            preinscripcion.TurnoId = hora;
+            preinscripcion.PersonaId = id;
+
+            var t = db.Turno
+                   .Where(b => b.TurnoId == preinscripcion.TurnoId)
+                   .FirstOrDefault();
+
+            var a = db.Alumno
+                   .Where(b => b.PersonaId == preinscripcion.PersonaId)
+                   .FirstOrDefault();
+
+            if ((t != null) & (a != null))
+            {
+                if ((fecha != null) & (hora != 0))
+                {
+                    if (ModelState.IsValid)
+                    {
+                        using (var ctx = new PreinscripcionContext())
+                        {
+                            ctx.PreinscripcionOnline.Add(preinscripcion);
+                            ctx.SaveChanges();
+
+                            var estNuevo = false;
+                            t.Estado = estNuevo;
+                            db.Entry(t).State = EntityState.Modified;
+                            db.SaveChanges();
+                        }
+                        return RedirectToAction("FinPreinscripcion", "Preinscripcion");
+                    }
+                }
+
+                else
+                {
+                    if (fecha == null)
+                    {
+                        TempData["mensajeerror"] = "Elija una fecha";
+                        return RedirectToAction("Turnera", "Turno");
+                    }
+
+                    if (hora == 0)
+                    {
+                        TempData["mensajeerror"] = "Elija un horario";
+                        return RedirectToAction("Turnera", "Turno");
+                    }
+
+                }
+
+
+                return View(new PreinscripcionOnline());
+
+            }
+            else
+            {
+                TempData["mensajeerror"] = "Error en la preinscripcion";
+                return RedirectToAction("Formulario", "Alumno");
+
+            }
+        }
+
         public ActionResult BuscarHorario(string id)
         {
             var types = HorarioList(id);
@@ -112,7 +192,7 @@ namespace Preinscripcion.Controllers
             }
         }
 
-        // GET: Turno/Edit/5
+    
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -120,10 +200,17 @@ namespace Preinscripcion.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             var estNuevo = true;
-            Turno turno = db.Turno.Find(id);
+            PreinscripcionOnline preins = db.PreinscripcionOnline.Find(id);
+            Turno turno = db.Turno.Find(preins.TurnoId);
+            Alumno alumno = db.Alumno.Find(preins.PersonaId);
 
-            if (turno != null)
+            if (preins != null)
             {
+
+                db.PreinscripcionOnline.Remove(preins);
+                db.Alumno.Remove(alumno);
+                db.SaveChanges();
+
                 turno.Estado = estNuevo;
                 db.Entry(turno).State = EntityState.Modified;
                 db.SaveChanges();
@@ -133,58 +220,43 @@ namespace Preinscripcion.Controllers
 
         }
 
-        public ActionResult Edit2(int? id1, int? id2)
+
+
+        public ActionResult Edit2([Bind(Include = "PersonaId, TurnoId")]  PreinscripcionOnline preinscripcion, int? idP, int idT)
         {
-            if (id1 == null)
+            if ((idP == null) || (idT == 0))
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                TempData["mensajeerror"] = "Elegí un Turno";
+                return RedirectToAction("ModificarTurno", "Turno");
             }
             var estViejo = true;
             var estNuevo = false;
 
-            Turno turnoViejo = db.Turno.Find(id1);
-            Turno turnoNuevo = db.Turno.Find(id2);
 
-            if (turnoViejo != null) 
+            PreinscripcionOnline preins = db.PreinscripcionOnline.Find(idP);
+            Turno turnoViejo = db.Turno.Find(preins.TurnoId);
+            Turno turnoNuevo = db.Turno.Find(idT);
+
+            if (preins != null)
             {
                 turnoViejo.Estado = estViejo;
                 db.Entry(turnoViejo).State = EntityState.Modified;
                 db.SaveChanges();
-            }
 
-            if (turnoNuevo != null)
-            {
                 turnoNuevo.Estado = estNuevo;
                 db.Entry(turnoViejo).State = EntityState.Modified;
                 db.SaveChanges();
+
+                //preins.TurnoId = hora;
+                db.Entry(preins).State = EntityState.Modified;
+                db.SaveChanges();
+
                 return RedirectToAction("ModificarTurnoOk");
             }
             return View();
 
         }
 
-
-        // POST: Turno/Edit/5
-        [HttpPost]
-        //public ActionResult Edit( int? id)
-        //{
-
-        //    //db.Entry(materiaComision).State = EntityState.Modified;
-        //    //db.SaveChanges();
-        //    // TODO: Add update logic here
-            
-        //        var estNuevo = true;
-        //        var turno = db.Turno.Where(b => b.TurnoId == id).FirstOrDefault();
-        //        if (turno != null)
-        //        {
-        //            turno.Estado = estNuevo;
-        //            db.Entry(turno).State = EntityState.Modified;
-        //            db.SaveChanges();
-        //            return RedirectToAction("Index");
-        //        }
-
-        //       return View("~/Views/Turno/CancelarTurnoOk.cshtml");
-        //}
 
         // GET: Turno/Delete/5
         public ActionResult Delete(int id)
